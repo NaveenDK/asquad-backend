29977441c06a13f141cf4d67f18f95087dd5ca29
Naveen Kolambage <naveenkolambage@gmail.com>
2023-11-05 16:55:32 +1300

    added minor fix

--- server.js
+++ server.js
@@ -1,53 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const mongoose = require("mongoose");
 const path = require("path");
-
+const { logger } = require("./middleware/logger");
 require("dotenv").config();
-
+const errorHandler = require("./middleware/errorHandler");
+const { error } = require("console");
+const connectDB = require("./config/dbConn");
 const app = express();
-const port = process.env.PORT;
-
-app.use(cors());
-
+const port = process.env.PORT || 3000;
+const { logEvents } = require("./middleware/logger");
+// app.use(cors());
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/groups", require("./routes/groupRoutes"));
+app.use("/cycles", require("./routes/cycleRoutes"));
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
-
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-  console.log("mongodb db connection established!!");
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
 });
-
-const cyclesRouter = require("./routes/cycles");
-
-app.use("/cycles", cyclesRouter);
-
-const adminsRouter = require("./routes/admins");
-
-app.use("/admins", adminsRouter);
-
-// const authRouter = require("./routes/auth");
-// app.use("/auth", authRouter);
-
-///---------deployment --------------------------
-
-// __dirname = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running..");
-//   });
-// }
-///---------deployment --------------------------
-
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+app.use(errorHandler);
+connectDB();
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
 });
